5c8db443dc003605a0f14646127d8cd8b52a00bf
Dakota Pierone <dakotapierone@yahoo.com>
2016-08-24 09:58:48 -0700

    Change Ultimate fist to be enchantible. Closes #22

--- src/main/java/com/grim3212/mc/pack/tools/items/ItemUltimateFist.java
+++ src/main/java/com/grim3212/mc/pack/tools/items/ItemUltimateFist.java
@@ -1,25 +1,26 @@
 package com.grim3212.mc.pack.tools.items;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import com.grim3212.mc.pack.core.item.ItemManual;
+import com.google.common.collect.Sets;
+import com.grim3212.mc.pack.core.manual.IManualEntry.IManualItem;
 import com.grim3212.mc.pack.core.manual.pages.Page;
 import com.grim3212.mc.pack.tools.client.ManualTools;
 import com.grim3212.mc.pack.tools.config.ToolsConfig;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.item.ItemTool;
 
-public class ItemUltimateFist extends ItemManual {
+public class ItemUltimateFist extends ItemTool implements IManualItem {
 
 	public ItemUltimateFist() {
-		setMaxStackSize(1);
+		super(ToolMaterial.DIAMOND, Sets.<Block> newHashSet());
 	}
 
 	@Override
@@ -44,9 +45,10 @@ public class ItemUltimateFist extends ItemManual {
 
 	@Override
 	public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
-		Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(slot, stack);
+		Multimap<String, AttributeModifier> multimap = HashMultimap.<String, AttributeModifier>create();
 		if (slot == EntityEquipmentSlot.MAINHAND) {
-			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", ToolsConfig.fistEntityDamage, 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", ToolsConfig.fistEntityDamage, 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", (double) this.attackSpeed, 0));
 		}
 		return multimap;
 	}
@@ -54,22 +56,5 @@ public class ItemUltimateFist extends ItemManual {
 	@Override
 	public float getStrVsBlock(ItemStack stack, IBlockState state) {
 		return ToolsConfig.fistBlockBreakSpeed;
-	}
-
-	@Override
-	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		stack.damageItem(2, target);
-		return true;
-	}
-
-	@Override
-	public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
-		stack.damageItem(1, entityLiving);
-		return true;
-	}
-
-	@Override
-	public boolean isFull3D() {
-		return true;
 	}
 }
