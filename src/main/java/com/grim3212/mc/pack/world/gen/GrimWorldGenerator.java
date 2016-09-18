37f7590478f5bf78d0b7da1f4c4ffd7c8a4c995e
Dakota Pierone <dakotapierone@yahoo.com>
2016-09-18 09:43:52 -0700

    Check to make sure biome isn't null when generating extras. Closes #39

--- src/main/java/com/grim3212/mc/pack/world/gen/GrimWorldGenerator.java
+++ src/main/java/com/grim3212/mc/pack/world/gen/GrimWorldGenerator.java
@@ -76,52 +76,54 @@ public class GrimWorldGenerator extends GrimWorldGen {
 	private void generateExtras(World world, Random random, int blockX, int blockZ) {
 		Biome biome = world.getBiomeProvider().getBiome(new BlockPos(blockX + 16, 60, blockZ + 16));
 
-		if (random.nextInt(WorldConfig.frequencyWheatField) == 0 && BiomeDictionary.isBiomeOfType(biome, Type.PLAINS)) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(40) + 60;
-				new WorldGenFarmland(random.nextInt(8) + 4, random.nextInt(6) + 2).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+		if (biome != null) {
+			if (random.nextInt(WorldConfig.frequencyWheatField) == 0 && BiomeDictionary.isBiomeOfType(biome, Type.PLAINS)) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(40) + 60;
+					new WorldGenFarmland(random.nextInt(8) + 4, random.nextInt(6) + 2).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
-		}
 
-		if (random.nextInt(WorldConfig.frequencySaplings) == 0 && !BiomeDictionary.isBiomeOfType(biome, Type.SANDY)) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(30) + 60;
-				new WorldGenSaplingsAndStumps(random.nextInt(16) + 1, true).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+			if (random.nextInt(WorldConfig.frequencySaplings) == 0 && !BiomeDictionary.isBiomeOfType(biome, Type.SANDY)) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(30) + 60;
+					new WorldGenSaplingsAndStumps(random.nextInt(16) + 1, true).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
-		}
 
-		if (random.nextInt(WorldConfig.frequencyTreeStumps) == 0 && !BiomeDictionary.isBiomeOfType(biome, Type.SANDY)) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(30) + 60;
-				new WorldGenSaplingsAndStumps(random.nextInt(12) + 3, false).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+			if (random.nextInt(WorldConfig.frequencyTreeStumps) == 0 && !BiomeDictionary.isBiomeOfType(biome, Type.SANDY)) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(30) + 60;
+					new WorldGenSaplingsAndStumps(random.nextInt(12) + 3, false).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
-		}
 
-		if (random.nextInt(WorldConfig.frequencySandstonePillars) == 0 && biome == Biomes.DESERT) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(25) + 60;
-				new WorldGenSandExpanded(random.nextInt(18) + 4, 0).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+			if (random.nextInt(WorldConfig.frequencySandstonePillars) == 0 && biome == Biomes.DESERT) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(25) + 60;
+					new WorldGenSandExpanded(random.nextInt(18) + 4, 0).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
-		}
 
-		if (random.nextInt(WorldConfig.frequencyCactusFields) == 0 && biome == Biomes.DESERT) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(55) + 60;
-				new WorldGenSandExpanded(random.nextInt(24) + 3, 1).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+			if (random.nextInt(WorldConfig.frequencyCactusFields) == 0 && biome == Biomes.DESERT) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(55) + 60;
+					new WorldGenSandExpanded(random.nextInt(24) + 3, 1).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
-		}
 
-		if (random.nextInt(WorldConfig.frequencySandPits) == 0 && biome == Biomes.DESERT) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(25) + 60;
-				new WorldGenSandExpanded(random.nextInt(19) + 4, 2).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+			if (random.nextInt(WorldConfig.frequencySandPits) == 0 && biome == Biomes.DESERT) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(25) + 60;
+					new WorldGenSandExpanded(random.nextInt(19) + 4, 2).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
-		}
 
-		if (random.nextInt(WorldConfig.frequencyMelons) == 0 && (biome == Biomes.RIVER || biome == Biomes.OCEAN || biome == Biomes.PLAINS)) {
-			for (int i = 0; i < 5; i++) {
-				int j = random.nextInt(10) + 60;
-				new WorldGenMelons(random.nextInt(8) + 4).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+			if (random.nextInt(WorldConfig.frequencyMelons) == 0 && (biome == Biomes.RIVER || biome == Biomes.OCEAN || biome == Biomes.PLAINS)) {
+				for (int i = 0; i < 5; i++) {
+					int j = random.nextInt(10) + 60;
+					new WorldGenMelons(random.nextInt(8) + 4).generate(world, random, new BlockPos(blockX + random.nextInt(16), j, blockZ + random.nextInt(16)));
+				}
 			}
 		}
 	}
