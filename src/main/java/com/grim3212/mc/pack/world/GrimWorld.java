f7073f13cdbb7d7116477fa31109d0b193edfb2e
Dakota Pierone <dakotapierone@yahoo.com>
2016-09-30 14:47:47 -0700

    Add support for Just Enough Resources for Mob Drops and Desert Wells

--- src/main/java/com/grim3212/mc/pack/world/GrimWorld.java
+++ src/main/java/com/grim3212/mc/pack/world/GrimWorld.java
@@ -5,18 +5,23 @@ import com.grim3212.mc.pack.core.part.GrimPart;
 import com.grim3212.mc.pack.core.proxy.CommonProxy;
 import com.grim3212.mc.pack.world.blocks.WorldBlocks;
 import com.grim3212.mc.pack.world.client.ManualWorld;
+import com.grim3212.mc.pack.world.compat.jer.JERWorld;
 import com.grim3212.mc.pack.world.config.WorldConfig;
 import com.grim3212.mc.pack.world.entity.WorldEntities;
+import com.grim3212.mc.pack.world.event.WorldPostInitEvent;
 import com.grim3212.mc.pack.world.gen.GrimWorldGenerator;
 import com.grim3212.mc.pack.world.gen.WorldEvents;
 import com.grim3212.mc.pack.world.items.WorldItems;
-import com.grim3212.mc.pack.world.util.DesertWellLoot;
+import com.grim3212.mc.pack.world.util.LootTables;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -44,7 +49,7 @@ public class GrimWorld extends GrimPart {
 
 		GameRegistry.registerWorldGenerator(new GrimWorldGenerator(), 5);
 		MinecraftForge.TERRAIN_GEN_BUS.register(new WorldEvents());
-		DesertWellLoot.init();
+		LootTables.initLootTables();
 
 		proxy.preInit();
 	}
@@ -53,6 +58,13 @@ public class GrimWorld extends GrimPart {
 	public void init(FMLInitializationEvent event) {
 		super.init(event);
 		proxy.initColors();
+
+		WorldEntities.addSpawns();
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.post(new WorldPostInitEvent());
 	}
 
 	@Override
@@ -64,5 +76,14 @@ public class GrimWorld extends GrimPart {
 	@SideOnly(Side.CLIENT)
 	public IManualPart getManual() {
 		return ManualWorld.INSTANCE;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Optional.Method(modid = "jeresources")
+	@SubscribeEvent
+	public void JERInit(WorldPostInitEvent evt) {
+		JERWorld jerWorld = new JERWorld();
+		MinecraftForge.EVENT_BUS.register(jerWorld);
+		jerWorld.register();
 	}
 }
---
+++ src/main/java/com/grim3212/mc/pack/world/event/WorldPostInitEvent.java
@@ -0,0 +1,15 @@
+package com.grim3212.mc.pack.world.event;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ * Blank event simply used for compatibility. This is called in PostInit for
+ * GrimWorld.
+ * 
+ * Useful for optional subscribeable methods see {@link com.grim3212.mc.pack.world.GrimWorld#JERInit(WorldPostInitEvent)}
+ * 
+ * @author Grim3212
+ */
+public class WorldPostInitEvent extends Event {
+
+}
