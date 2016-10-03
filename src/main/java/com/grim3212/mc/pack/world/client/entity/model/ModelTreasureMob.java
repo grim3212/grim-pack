22fd8b1d8d5d5162d98e857979c97722f5731c37
Grim3212 <dakotapierone@yahoo.com>
2016-10-03 10:03:46 -0700

    Fix TreasureMobs faces to be the opened part of the chest.

--- src/main/java/com/grim3212/mc/pack/world/client/entity/model/ModelTreasureMob.java
+++ src/main/java/com/grim3212/mc/pack/world/client/entity/model/ModelTreasureMob.java
@@ -2,6 +2,7 @@ package com.grim3212.mc.pack.world.client.entity.model;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -18,10 +19,14 @@ public class ModelTreasureMob extends ModelBase {
 		box0.setRotationPoint(-6F, 16F, -6F);
 	}
 
-	public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7) {
+	@Override
+	public void render(Entity entity, float par2, float par3, float par4, float par5, float par6, float par7) {
+		GlStateManager.pushMatrix();
+		GlStateManager.rotate(180f, 0.0f, 1.0F, 0.0F);
 		setRotationAngles(par2, par3, par4, par5, par6, par7);
 		box1.render(par7);
 		box0.render(par7);
+		GlStateManager.popMatrix();
 	}
 
 	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5) {
