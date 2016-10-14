1a288f5c02fe21ed78529dd7d31518e231116249
Grim3212 <dakotapierone@yahoo.com>
2016-10-14 08:58:48 -0700

    Change Infinite Lava to use CreateFluidSourceEvent. Closes #47

--- src/main/java/com/grim3212/mc/pack/util/event/BlockChangeEvents.java
+++ src/main/java/com/grim3212/mc/pack/util/event/BlockChangeEvents.java
@@ -4,17 +4,12 @@ import com.grim3212.mc.pack.util.config.UtilConfig;
 
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.BlockFenceGate;
-import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.BlockTrapDoor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class BlockChangeEvents {
@@ -36,44 +31,11 @@ public class BlockChangeEvents {
 	}
 
 	@SubscribeEvent
-	public void neighborNotifyEvent(BlockEvent.NeighborNotifyEvent event) {
+	public void createInfiniteLava(BlockEvent.CreateFluidSourceEvent event) {
 		if (UtilConfig.infiniteLava) {
 			if (event.getState().getMaterial() == Material.LAVA) {
-
-				int adjacent = checkAdjacentBlock(event.getWorld(), event.getPos());
-
-				if (adjacent >= 2) {
-					event.getWorld().setBlockState(event.getPos(), Blocks.FLOWING_LAVA.getDefaultState());
-				}
-
-				for (EnumFacing enumfacing : EnumFacing.Plane.HORIZONTAL) {
-					BlockPos newPos = event.getPos().offset(enumfacing);
-
-					if (event.getWorld().getBlockState(newPos).getMaterial() == Material.LAVA) {
-						int adjacent2 = checkAdjacentBlock(event.getWorld(), newPos);
-
-						if (adjacent2 >= 2) {
-							event.getWorld().setBlockState(newPos, Blocks.FLOWING_LAVA.getDefaultState());
-						}
-					}
-				}
+				event.setResult(Result.ALLOW);
 			}
 		}
-	}
-
-	private int checkAdjacentBlock(World worldIn, BlockPos pos) {
-		int adjacentBlocks = 0;
-		for (EnumFacing enumfacing : EnumFacing.Plane.HORIZONTAL) {
-			BlockPos newPos = pos.offset(enumfacing);
-
-			if (this.getLevel(worldIn, newPos) == 0) {
-				++adjacentBlocks;
-			}
-		}
-		return adjacentBlocks;
-	}
-
-	private int getLevel(IBlockAccess worldIn, BlockPos pos) {
-		return worldIn.getBlockState(pos).getMaterial() == Material.LAVA ? worldIn.getBlockState(pos).getValue(BlockLiquid.LEVEL) : -1;
 	}
 }
