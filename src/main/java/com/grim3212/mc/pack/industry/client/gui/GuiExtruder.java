a834d8c49b97f795bdf566175757a2e2cd30c515
Scott Hendrickson <scottohendrickson@yahoo.com>
2016-12-04 18:12:07 -0600

    Number of blocks added to Extruder GUI

--- src/main/java/com/grim3212/mc/pack/industry/client/gui/GuiExtruder.java
+++ src/main/java/com/grim3212/mc/pack/industry/client/gui/GuiExtruder.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import com.grim3212.mc.pack.GrimPack;
 import com.grim3212.mc.pack.core.network.PacketDispatcher;
+import com.grim3212.mc.pack.industry.config.IndustryConfig;
 import com.grim3212.mc.pack.industry.inventory.ContainerExtruder;
 import com.grim3212.mc.pack.industry.inventory.InventoryExtruder;
 import com.grim3212.mc.pack.industry.network.MessageExtruderDirection;
@@ -61,7 +62,12 @@ public class GuiExtruder extends GuiContainer {
 		this.fontRendererObj.drawString(this.playerInv.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
 
 		if (extruderInv.getExtruder() != null) {
-			this.fontRendererObj.drawString(I18n.format("container.extruder.fuel", extruderInv.getExtruder().getFuelAmount()), 102, 24, 0x404040);
+			this.fontRendererObj.drawString(I18n.format("container.extruder.fuel", extruderInv.getExtruder().getFuelAmount()), 10, 24, 0x404040);
+			if (extruderInv.getStackInSlot(0) != null) {
+				int stackSize = extruderInv.getExtruderSlotStackSize(0);
+				this.fontRendererObj.drawString(I18n.format("container.extruder.exblocks", (extruderInv.getExtruder().getFuelAmount() * (stackSize + 1)) / IndustryConfig.fuelPerExtrudedBlock), 102, 18, 0x404040);
+				this.fontRendererObj.drawString(I18n.format("container.extruder.mineblocks", (extruderInv.getExtruder().getFuelAmount() * (stackSize + 1)) / IndustryConfig.fuelPerMinedBlock), 102, 32, 0x404040);
+			}
 		}
 	}
 
