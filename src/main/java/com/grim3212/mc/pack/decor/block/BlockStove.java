3e6699796c20323ecaa90b144cb50d4b53ae2264
Dakota Pierone <dakotapierone@yahoo.com>
2017-02-25 11:49:36 -0800

    Make stoves emit smoke with chimneys

--- src/main/java/com/grim3212/mc/pack/decor/block/BlockStove.java
+++ src/main/java/com/grim3212/mc/pack/decor/block/BlockStove.java
@@ -1,13 +1,30 @@
 package com.grim3212.mc.pack.decor.block;
 
+import java.util.Random;
+
 import com.grim3212.mc.pack.core.manual.pages.Page;
+import com.grim3212.mc.pack.decor.GrimDecor;
 import com.grim3212.mc.pack.decor.client.ManualDecor;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockStove extends BlockFireplaceBase {
 
 	public BlockStove() {
+	}
+	
+	@Override
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		if (worldIn.getBlockState(pos).getValue(ACTIVE) && worldIn.getBlockState(pos.up()).getBlock() == DecorBlocks.chimney) {
+			int smokeheight = 1;
+			while (worldIn.getBlockState(pos.up(smokeheight)).getBlock() == DecorBlocks.chimney) {
+				smokeheight++;
+			}
+
+			GrimDecor.proxy.produceSmoke(worldIn, pos.up(smokeheight), 0.5D, 0.0D, 0.5D, 1, true);
+		}
 	}
 
 	@Override
