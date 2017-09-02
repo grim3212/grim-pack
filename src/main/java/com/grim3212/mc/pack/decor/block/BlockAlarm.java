eecb6879773cb83eecb447d88d799af1e83bd462
Dakota Pierone <dakotapierone@yahoo.com>
2017-09-02 07:06:54 -0700

    Alarms can be placed on any face

--- src/main/java/com/grim3212/mc/pack/decor/block/BlockAlarm.java
+++ src/main/java/com/grim3212/mc/pack/decor/block/BlockAlarm.java
@@ -14,9 +14,12 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
@@ -30,7 +33,14 @@ import net.minecraft.world.World;
 public class BlockAlarm extends BlockManual implements ITileEntityProvider {
 
 	public static final PropertyBool POWERED = PropertyBool.create("powered");
-	private static final AxisAlignedBB BOUNDS = new AxisAlignedBB(0.1875f, 0.0f, 0.1875f, 0.8125f, 0.25f, 0.8125f);
+	public static final PropertyEnum<EnumFacing> FACING = PropertyEnum.create("facing", EnumFacing.class);
+
+	private static final AxisAlignedBB UP_BOUNDS = new AxisAlignedBB(0.1875f, 0.0f, 0.1875f, 0.8125f, 0.25f, 0.8125f);
+	private static final AxisAlignedBB DOWN_BOUNDS = new AxisAlignedBB(0.1875f, 0.75f, 0.1875f, 0.8125f, 1.0f, 0.8125f);
+	private static final AxisAlignedBB NORTH_BOUNDS = new AxisAlignedBB(0.1875f, 0.1875f, 0.75f, 0.8125f, 0.8125f, 1.0f);
+	private static final AxisAlignedBB SOUTH_BOUNDS = new AxisAlignedBB(0.1875f, 0.1875f, 0.0f, 0.8125f, 0.8125f, 0.25f);
+	private static final AxisAlignedBB WEST_BOUNDS = new AxisAlignedBB(0.75f, 0.1875f, 0.1875f, 1.0f, 0.8125f, 0.8125f);
+	private static final AxisAlignedBB EAST_BOUNDS = new AxisAlignedBB(0.0f, 0.1875f, 0.1875f, 0.25f, 0.8125f, 0.8125f);
 
 	public BlockAlarm() {
 		super("alarm", Material.IRON, SoundType.METAL);
@@ -42,7 +52,7 @@ public class BlockAlarm extends BlockManual implements ITileEntityProvider {
 
 	@Override
 	protected IBlockState getState() {
-		return super.getState().withProperty(POWERED, false);
+		return super.getState().withProperty(POWERED, false).withProperty(FACING, EnumFacing.NORTH);
 	}
 
 	@Override
@@ -52,7 +62,22 @@ public class BlockAlarm extends BlockManual implements ITileEntityProvider {
 
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return BOUNDS;
+		switch (state.getValue(FACING)) {
+		case DOWN:
+			return DOWN_BOUNDS;
+		case EAST:
+			return EAST_BOUNDS;
+		case NORTH:
+			return NORTH_BOUNDS;
+		case SOUTH:
+			return SOUTH_BOUNDS;
+		case UP:
+			return UP_BOUNDS;
+		case WEST:
+			return WEST_BOUNDS;
+		}
+
+		return FULL_BLOCK_AABB;
 	}
 
 	@Override
@@ -72,15 +97,23 @@ public class BlockAlarm extends BlockManual implements ITileEntityProvider {
 
 	@Override
 	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-		return !worldIn.isAirBlock(pos.down()) && worldIn.getBlockState(pos.down()).isFullCube();
+		return worldIn.getBlockState(pos.west()).isOpaqueCube() ? true : (worldIn.getBlockState(pos.east()).isOpaqueCube() ? true : (worldIn.getBlockState(pos.north()).isOpaqueCube() ? true : (worldIn.getBlockState(pos.south()).isOpaqueCube() ? true : (worldIn.getBlockState(pos.up()).isOpaqueCube() ? true : worldIn.getBlockState(pos.down()).isOpaqueCube()))));
+	}
+
+	@Override
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+		return world.isSideSolid(pos.offset(facing.getOpposite()), facing, true) ? this.getDefaultState().withProperty(FACING, facing) : this.getDefaultState().withProperty(FACING, EnumFacing.DOWN);
+	}
+
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+		this.tryDropAlarm(worldIn, pos);
 	}
 
 	@Override
 	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
 		// Drop block if it has no base
-		if (!canPlaceBlockAt(worldIn, pos)) {
-			this.dropBlockAsItem(worldIn, pos, state, 0);
-			worldIn.setBlockToAir(pos);
+		if (tryDropAlarm(worldIn, pos)) {
 			return;
 		}
 
@@ -123,18 +156,39 @@ public class BlockAlarm extends BlockManual implements ITileEntityProvider {
 		return new TileEntityAlarm();
 	}
 
+	public static EnumFacing getFacing(int meta) {
+		return EnumFacing.getFront(meta & 7);
+	}
+
 	@Override
 	public IBlockState getStateFromMeta(int meta) {
-		return this.getDefaultState().withProperty(POWERED, meta == 1 ? true : false);
+		return this.getDefaultState().withProperty(FACING, getFacing(meta)).withProperty(POWERED, (meta & 8) > 0);
 	}
 
 	@Override
 	public int getMetaFromState(IBlockState state) {
-		return state.getValue(POWERED) ? 1 : 0;
+		byte b0 = 0;
+		int i = b0 | state.getValue(FACING).getIndex();
+
+		if (state.getValue(POWERED)) {
+			i |= 8;
+		}
+
+		return i;
 	}
 
 	@Override
 	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, new IProperty[] { POWERED });
+		return new BlockStateContainer(this, new IProperty[] { FACING, POWERED });
+	}
+
+	private boolean tryDropAlarm(World worldIn, BlockPos pos) {
+		if (!canPlaceBlockAt(worldIn, pos)) {
+			this.dropBlockAsItem(worldIn, pos, worldIn.getBlockState(pos), 0);
+			worldIn.setBlockToAir(pos);
+			return true;
+		}
+
+		return false;
 	}
 }
